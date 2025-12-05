f7cec0efe1e42c413d8837ca02311470d8f23017
Jodie Weldon <weldon.jodie@gmail.com>
2025-12-04 16:26:48 -0800

    Day 2 part 2

--- clojure-2025/src/aoc/day2.clj
+++ clojure-2025/src/aoc/day2.clj
@@ -24,30 +24,70 @@
 (defn invalid-id? [id] (let [id-str (str id)
                              length (count id-str)
                              half (/ length 2)
-                             start (subs (str id) 0 half)
-                             end (subs (str id) half)]
+                             start (subs id-str 0 half)
+                             end (subs id-str half)]
                          (= start end)))
 
-(invalid-id? "1010")
+(invalid-id? "10101")
+(subs "10101" (/ (count "10101") 2))
 
 (defn filter-invalid-ids [ids] (filter invalid-id? ids))
 
 (filter-invalid-ids '(11 12 13 14 15 16 17 18 19 20 21 22))
 
-(defn ->invaid-ids [data] (mapcat filter-invalid-ids data))
+(defn invaid-ids [data] (mapcat filter-invalid-ids data))
 
 (defn sum [ids] (apply + ids))
 
 (defn find-invalid-ids [data]
   (->> data
        str/trim
-       split-ranges 
-       ->int-ranges 
-       ->lazy-ranges 
-       ->invaid-ids 
-       sum
-       ))
+       split-ranges
+       ->int-ranges
+       ->lazy-ranges
+       invaid-ids
+       sum))
 
 (find-invalid-ids example-data)
 
 (find-invalid-ids (slurp "data/day_2.txt"))
+
+(defn invalid-id-2? [id]
+  (let [id-str (str id)
+        id-length (count id-str)
+        range-to-half (range 1 (inc (/ id-length 2)))
+        to-partitions #(partition % % nil id-str)
+        invalid? #(apply = %)]
+    (if (<= id-length 1) false
+        (some invalid? (map to-partitions range-to-half)))))
+
+(invalid-id-2? 12341234)
+(invalid-id-2? 123123123)
+(invalid-id-2? 1212121212)
+(invalid-id-2? 1111111)
+(invalid-id-2? 100N)
+(invalid-id-2? 2)
+
+(defn filter-invalid-ids-2 [ids]
+  (let [id-range (str (first ids) "-" (last ids))
+        filtered (filter invalid-id-2? ids)]
+    (println id-range "has" (count filtered) "invalid IDs," filtered)
+    filtered))
+
+(filter-invalid-ids-2 '(11 12 13 14 15 16 17 18 19 20 21 22))
+(filter-invalid-ids-2 '(95 96N 97N 98N 99N 100N 101N 102N 103N 104N 105N 106N 107N 108N 109N 110N 111N 112N 113N 114N 115N))
+
+(defn invalid-ids-2 [id-ranges] (mapcat filter-invalid-ids-2 id-ranges))
+
+(defn find-invalid-ids-2 [data]
+  (->> data
+       str/trim
+       split-ranges
+       ->int-ranges
+       ->lazy-ranges
+       invalid-ids-2
+       sum))
+
+(find-invalid-ids-2 example-data)
+
+(find-invalid-ids-2 (slurp "data/day_2.txt"))
